2d4269e6c6f3de86bace520e50cb0231b8f7c36d
puki <punitp07o7@gmail.com>
2023-12-09 22:00:06 +0530

    setting up footer

--- fintra/appoffin/models.py
+++ fintra/appoffin/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Service(models.Model):
     service_title = models.CharField(max_length=100)
     service_desc = models.CharField(max_length=400)
+    logos = models.ImageField(upload_to="finance/services_logos", default="")
 
     def __str__(self):
         return self.service_title
